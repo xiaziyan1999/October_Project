ea231800c8239cb719fb6c76ba660991ad9c52b3
mgraves123 <61752707+mgraves123@users.noreply.github.com>
2021-10-12 11:52:50 -0400

    Update rfile.R

---
+++ rfile.R
@@ -0,0 +1,38 @@
+```{r}
+library(Hmisc)
+```
+
+
+```{r}
+movies = read.csv("movies.csv", header = TRUE) %>% 
+  mutate(title = as.character(title),
+         domgross = as.numeric(as.character(domgross)),
+         intgross = as.numeric(as.character(intgross)),
+         domgross_2013. = as.numeric(as.character(domgross_2013.)),
+         intgross_2013. = as.numeric(as.character(intgross_2013.)),
+         decade = floor(2013/10)*10,
+         domprofit_2013 = domgross_2013.*.5,
+         intprofit_2013 = intgross_2013.*.4,
+         returns_2013 = (domprofit_2013+intprofit_2013)-budget_2013.,
+         budget_size_2013 = )
+```
+
+```{r}
+#data transformations
+
+hist.data.frame(movies[, -c(2,3)])
+pairs(movies[, -c(2:6, 10)])
+
+movies_transformed = movies %>% 
+  mutate(year = (year)^2,
+         budget = log(budget),
+         domgross = log(domgross),
+         intgross = log(intgross), 
+         budget_2013. = log(budget_2013.),
+         domgross_2013. = log(domgross_2013.),
+         intgross_2013. = log(intgross_2013.))
+
+hist.data.frame(movies_transformed[, -c(2,3)])
+pairs(movies_transformed[, -c(2:6, 10)])
+```
+
