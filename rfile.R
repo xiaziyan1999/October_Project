1815fe95f104d3a984a6e03895c11f0a91495e4e
Ziyan Xia <56137616+xiaziyan1999@users.noreply.github.com>
2021-10-12 12:08:39 -0400

    Update rfile.R

--- rfile.R
+++ rfile.R
@@ -1,9 +1,6 @@
-```{r}
+
 library(Hmisc)
-```
 
-
-```{r}
 movies = read.csv("movies.csv", header = TRUE) %>% 
   mutate(title = as.character(title),
          domgross = as.numeric(as.character(domgross)),
@@ -15,9 +12,7 @@ movies = read.csv("movies.csv", header = TRUE) %>%
          intprofit_2013 = intgross_2013.*.4,
          returns_2013 = (domprofit_2013+intprofit_2013)-budget_2013.,
          budget_size_2013 = cut(x[,7],c(0,25000000,50000000,75000000,100000000,125000000,150000000,175000000,200000000,225000000,250000000,275000000),labels=F)
-```
 
-```{r}
 #data transformations
 
 hist.data.frame(movies[, -c(2,3)])
