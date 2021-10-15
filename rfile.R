4a77f6c559a24f6b016bcf42bf6a5b9f44eaf73d
mgraves123 <61752707+mgraves123@users.noreply.github.com>
2021-10-15 11:40:44 -0400

    Update rfile.R

--- rfile.R
+++ rfile.R
@@ -13,27 +13,9 @@ movies = read.csv("movies.csv", header = TRUE) %>%
          decade = floor(year/10)*10,
          domprofit_2013 = domgross_2013.*.5,
          intprofit_2013 = intgross_2013.*.4,
-         returns_2013 = (domprofit_2013+intprofit_2013)-budget_2013.,
-         budget_size_2013 = as.factor(cut(budget_2013.,c(0,1000000,20000000,50000000,100000000,500000000),labels=F)))
-```
-
-```{r}
-#data transformations
-
-hist.data.frame(movies[, -c(2,3)])
-pairs(movies[, -c(2:6, 10)])
-
-movies_transformed = movies %>% 
-  mutate(year = (year)^2,
-         budget = log(budget),
-         domgross = log(domgross),
-         intgross = log(intgross), 
-         budget_2013. = log(budget_2013.),
-         domgross_2013. = log(domgross_2013.),
-         intgross_2013. = log(intgross_2013.))
-
-hist.data.frame(movies_transformed[, -c(2,3)])
-pairs(movies_transformed[, -c(2:6, 10)])
+         profit_2013 = (domprofit_2013+intprofit_2013)-budget_2013.,
+         budget_size_2013 = as.factor(cut(budget_2013.,c(0,1000000,20000000,50000000,100000000,500000000),labels=F)),
+         ROI = profit_2013/budget_2013.)
 ```
 
 ```{r}
